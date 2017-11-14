09b0f3e10f827d77c588600cd388e7cf2a8143de
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-14 00:03:02 -0200

    Criacao Variavel Conexao JDNI-datasource

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,9 @@
+package br.com.framework.implementacao.crud;
+
+public class VariavelConexaoUtil {
+	/**
+	 * 
+	 * Nome do caminho do JDNI do datasource Tomcat
+	 */
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
