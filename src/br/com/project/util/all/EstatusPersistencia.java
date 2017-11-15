b8a407cf20faa272f61ef4e66c5bdd5b22179dcf
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-15 19:51:06 -0200

    Criacao Messagens do sistema

---
+++ src/br/com/project/util/all/EstatusPersistencia.java
@@ -0,0 +1,18 @@
+package br.com.project.util.all;
+
+public enum EstatusPersistencia {
+
+	ERRO("Erro"),SUCESSO("Sucesso"),
+	OBJETO_REFERENCIADO("Esse objeto não pode ser apagado por possuir referencias ao mesmo");
+	
+	private String name;
+	
+	private EstatusPersistencia(String s) {
+		this.name = s;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+}
