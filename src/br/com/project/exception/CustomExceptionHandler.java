f88a7560bb67b91eefdb9999fa9884a07118bd61
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-19 21:06:04 -0200

    Tratamento de exceção parte

--- src/br/com/project/exception/CustomExceptionHandler.java
+++ src/br/com/project/exception/CustomExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
 import javax.faces.application.NavigationHandler;
 import javax.faces.context.ExceptionHandler;
 import javax.faces.context.ExceptionHandlerWrapper;
@@ -12,6 +13,7 @@ import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 
 import org.hibernate.SessionFactory;
+import org.primefaces.context.RequestContext;
 
 import br.com.framework.hibernate.session.HibernateUtil;
 
@@ -66,11 +68,37 @@ public class CustomExceptionHandler extends ExceptionHandlerWrapper {
 			// Trabalhando a exeption
 			try {
 				requestMap.put("exceptionMessage", exeption.getMessage());
-				
+				//Não pode ser removido por ter chave estranjeira
 				if(exeption!= null && exeption.getMessage() !=null
 						&& exeption.getMessage().indexOf("ConstraintViolationException")!=-1){
 					
+					FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage(FacesMessage
+							.SEVERITY_WARN,"Registro não pode ser removido por estar associado.",""));
+				} else 	if(exeption!= null && exeption.getMessage() !=null
+						&& exeption.getMessage().indexOf("org.hibernate.StaleObjectStateException")!=-1){
+					
+					FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage(FacesMessage
+							.SEVERITY_ERROR,"Registro foi atualizado ou excluido por outro usuario. Consulte novamente.",""));
+				} else{
+					//Informa o usuario do erro
+					FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage(FacesMessage
+							.SEVERITY_FATAL,"O sistema se recuperou de um erro inesperado.",""));
+					FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage(FacesMessage
+							.SEVERITY_INFO,"Você pode contiunar usando o sistema normalmente.",""));
+					FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage(FacesMessage
+							.SEVERITY_FATAL,"Erro foi causado por:\n"+exeption.getMessage(),""));
+
+					//PrimeFaces
+					//Alert apenas é exibido se a pagina nõ redirecionar
+					RequestContext.getCurrentInstance().execute("alert('O sistema se recuperou de um erro inseperado')");
+					
+					RequestContext.getCurrentInstance().
+					showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_INFO,"Erro","O sistema se recuperou de um erro inseperado"));
+					//Redireciona para a pagina de erro
+					navigationHandler.handleNavigation(facesContext, null, "/error/erro.jsf?faces-redirect=true&expired=true");
 				}
+				//Renderiaza a pagina de erro e exibe as messagens
+				facesContext.renderResponse();
 
 			} finally {
 				SessionFactory sf = HibernateUtil.getSessionFactory();
