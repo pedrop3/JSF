8072f974990064c51ddf2200018c5b0982ecf3e2
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-20 15:34:37 -0200

    Configuração ContextLoaderListenerUtils

---
+++ src/br/com/project/listener/ContextLoaderListenerUtils.java
@@ -0,0 +1,27 @@
+package br.com.project.listener;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+@ApplicationScoped
+public class ContextLoaderListenerUtils extends ContextLoader implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	//Irá retorna todo contexto do ambiente
+	private static WebApplicationContext getWac(){
+		return WebApplicationContextUtils.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+	}
+
+	public static Object getBean(String idNomeBean){
+		return getWac().getBean(idNomeBean);
+	}
+	//
+	public static Object getBean(Class<?> classe){
+		return getWac().getBean(classe);
+	}
+}
